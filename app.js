2c687421e99103523583766e912c81f5c2e8c30b
aaron <aaron@surfboard.se>
2023-12-11 10:42:14 +0530

    added cart data model

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ Cart.belongsToMany(Product, { through : CartItem });
 Product.belongsToMany(Cart, { through : CartItem});
 
 sequelize
-    .sync({force : true })
-    //.sync()
+    //.sync({force : true })
+    .sync()
     .then(results => {
         return User.findByPk(1);
     //console.log(results);
